6ef6e0bb19026f82d6222cc9c1b1444575060b95
viktorevstigneev <viktor_evstigneev14@bk.ru>
2020-03-09 23:20:06 +0300

    change script after first review

--- js/main.js
+++ js/main.js
@@ -1,60 +1,63 @@
-
-  `use strict`;
+'use strict ';
   const balance = document.querySelector('.Expense__caption');
   const minus = document.querySelector('.Expense__minus');
   const plus = document.querySelector('.Expense__plus');
-  const list=document.querySelector('.Expence__list');
-  const text=document.querySelector('.Text');
-  const number=document.querySelector('.Number');
- const submit=document.querySelector('.Expence__button');
+  const list = document.querySelector('.Expence__list');
+  const text = document.querySelector('.Text');
+  const number = document.querySelector('.Number');
+  const submit = document.querySelector('.Expence__button');
 
- let score=0;
-let transactions=[];
-let item;
-let i=0;
-
+  let transactions = [];
+  let score = 0;
+  let item;
+  let i = 0;
  
- function Remove(index) {
-  transactions.forEach(function(item, i, transactions) {
-    if(item.index!==index){
-    score+=item.amount;}
+
+const UpdateValues = ()=> {
+  transactions.forEach(function(item) {
+    score += item.amount;
   });
-  balance.innerHTML="$"+score;
-  score=0;
+  balance.innerHTML = "$" + score;
+  score = 0;
 }
- const Add =()=>{
-     item={
-      index:i++,
-      record: text.value,
-      amount:Number(number.value)
-    };
-    transactions.push(item);
-    transactions.forEach(function(item, i, transactions) {
-      score+=item.amount;
-    });
-   balance.innerHTML="$"+score;
-   score=0;
-   console.log('transactions: ', transactions);
-  let li=document.createElement('li');
-  li.innerHTML=`
-  
-      <button class="delete-btn" onclick="Remove(${item.index})">
-        x
+ 
+ const AddTransaction =()=>{
+  item={
+    index: i++,
+    record: text.value,
+    amount: Number(number.value)
+  };
+  transactions.push(item);
+ }
+
+  const AddTransactionToDOM = () => {
+    let li = document.createElement('li');
+    li.innerHTML =`
+      <button class="delete-btn" onclick="RemoveTransaction(${item.index})">
+       x
       </button>
       ${item.record}
       <span>${item.amount}</span> 
-  `;    
-  list.appendChild(li);
- }
+    `;    
+    list.appendChild(li);
+    console.log('li: ', li);
+  }
 
-//add to Dom
+  function RemoveTransaction (index) {
+    transactions = transactions.filter( item =>  item.index !== index  ); 
+    UpdateValues();
+  }
+  
   submit.addEventListener('click', () => {
-   if(text.value==""||number.value==""){
-     alert("value is empty");
+   if( text.value === "" || number.value === "" ){
+     alert( "value is empty" );
    }
    else{
-     Add();
-   
+    AddTransaction();
+    AddTransactionToDOM();
+    UpdateValues();
+    text.value = "";
+    number.value = "";
    }
 
    });
